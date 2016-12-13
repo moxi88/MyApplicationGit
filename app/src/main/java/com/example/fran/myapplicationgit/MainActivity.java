3048cc387218eea2903dd00c83568d9c91a8f8c0
moxi88 <fran_mno@hotmail.com>
2016-12-13 09:30:59 +0100

    Cambios fragment

--- app/src/main/java/com/example/fran/myapplicationgit/MainActivity.java
+++ app/src/main/java/com/example/fran/myapplicationgit/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.fran.myapplicationgit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import layout.Fragment1;
+import layout.Fragment2;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        WindowManager wm = getWindowManager();
+        Display d = wm.getDefaultDisplay();
+
+        if (d.getRotation() == Surface.ROTATION_90){
+            Fragment1 fragment1 = new Fragment1();
+            fragmentTransaction.replace(android.R.id.content, fragment1).commit();
+        }else {
+            Fragment2 fragment2 = new Fragment2();
+            fragmentTransaction.replace(android.R.id.content, fragment2).commit();
+        }
+
     }
+
 }
